71a3e7cfe0dd42b49e6455108c05789e3c544962
heover1cks <heover1cks@gmail.com>
2023-01-19 17:44:49 +0900

    improve password generation function

--- pkg/password/password.go
+++ pkg/password/password.go
@@ -1,18 +1,23 @@
 package password
 
 import (
+	cryptoRand "crypto/rand"
 	"gonum.org/v1/gonum/stat/combin"
 	"idpassgen/pkg/utils"
 	"log"
+	"math"
+	"math/big"
 	"math/rand"
+
 	"time"
 )
 
 const (
-	alphabetUpperCaseBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	alphabetLowerCaseBytes = "abcdefghijklmnopqrstuvwxyz"
-	numberBytes            = "1234567890"
-	specialCharacterBytes  = "!@#$%^&*"
+	alphabetUpperCaseBytes        = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	alphabetLowerCaseBytes        = "abcdefghijklmnopqrstuvwxyz"
+	numberBytes                   = "1234567890"
+	specialCharacterBytes         = "!@#$%^&*<>?-=_+"
+	extendedSpecialCharacterBytes = "~!@#$%^&*()_+`-={}|[]\\\\:\\\"<>?,./"
 )
 
 type Options struct {
@@ -59,6 +64,113 @@ func (p *Options) WithMaxNumbers(maxNumbers int) {
 }
 func (p *Options) WithMaxSpecialCharacters(maxSpecialCharacters int) {
 	p.maxSpecialCharacters = maxSpecialCharacters
+}
+
+type Password struct {
+	inserted  []bool
+	generated []byte
+	seed      int64
+	length    int
+}
+
+func NewPassword(initial []byte, length int, seed int64) *Password {
+	return &Password{
+		inserted:  make([]bool, length),
+		generated: initial,
+		seed:      seed,
+		length:    length,
+	}
+}
+func (p *Password) Get() string {
+	return string(p.generated)
+}
+
+// TODO: need to check what is better if Password contains seed value or generate seed everytime insertAtRandomPosition is called
+func (p *Password) insertAtRandomPosition(target byte) {
+	pos := rand.Int() % p.length
+	if p.inserted[pos] {
+		p.insertAtRandomPosition(target)
+	} else {
+		front := append(p.generated[:pos], target)
+		if pos == p.length-1 {
+			p.generated = front
+		} else {
+			back := p.generated[pos+1:]
+			p.generated = append(front, back...)
+		}
+	}
+}
+
+func (p *Options) GeneratePasswordV3() (Pass string) {
+	seed, _ := cryptoRand.Int(cryptoRand.Reader, big.NewInt(math.MaxInt64))
+	rand.Seed(seed.Int64())
+	if !p.includeUpperCase && !p.includeLowerCase && !p.includeNumbers && !p.includeSpecialCharacters {
+		log.Fatalf("all options are disabled")
+	}
+	s := p.SeparationCounter()
+	c := combin.Combinations(p.passLength, s-1)
+
+	var countForEach []int
+	prev := 0
+	selected := c[rand.Intn(len(c))]
+	for _, v := range selected {
+		countForEach = append(countForEach, v-prev+1)
+		prev = v + 1
+	}
+	countForEach = append(countForEach, p.passLength-prev+1)
+
+	var others []byte
+	var alphabets int
+	var v int
+	var pw *Password
+
+	if p.includeNumbers {
+		// Cap number count to max
+		v, countForEach = utils.Shift(countForEach)
+		if v > p.maxNumbers && p.maxNumbers >= 0 {
+			countForEach[0] += v - p.maxNumbers
+			v = p.maxNumbers
+		}
+		// append number bytes
+		others = utils.RandStringBytes(v, []byte(numberBytes))
+	}
+
+	if p.includeSpecialCharacters {
+		v, countForEach = utils.Shift(countForEach)
+
+		// Cap special character count to max
+		if v > p.maxSpecialCharacters && p.maxSpecialCharacters >= 0 {
+			countForEach[0] += v - p.maxSpecialCharacters
+			v = p.maxSpecialCharacters
+		}
+		// append special character bytes
+		others = append(others, utils.RandStringBytes(v, []byte(specialCharacterBytes))...)
+	}
+	if p.includeUpperCase || p.includeLowerCase {
+		var alphabetByte []byte
+		switch {
+		case p.includeUpperCase && p.includeLowerCase:
+			alphabetByte = []byte(alphabetUpperCaseBytes + alphabetLowerCaseBytes)
+			break
+		case p.includeUpperCase && !p.includeLowerCase:
+			alphabetByte = []byte(alphabetUpperCaseBytes)
+			break
+		case !p.includeUpperCase && p.includeLowerCase:
+			alphabetByte = []byte(alphabetLowerCaseBytes)
+			break
+		default:
+			panic("")
+		}
+		for _, i := range countForEach {
+			alphabets += i
+		}
+
+		pw = NewPassword(utils.RandStringBytes(alphabets+len(others), alphabetByte), p.passLength, seed.Int64())
+		for _, char := range others {
+			pw.insertAtRandomPosition(char)
+		}
+	}
+	return pw.Get()
 }
 
 func (p *Options) GeneratePassword() (Pass string) {
--- pkg/password/password_test.go
+++ pkg/password/password_test.go
@@ -1,11 +1,37 @@
 package password
 
-import "testing"
+import (
+	"sync"
+	"testing"
+	"time"
+)
 
 func TestGeneratePass(t *testing.T) {
+	wg := sync.WaitGroup{}
+
 	opts := NewOptions()
-	opts.WithPassLength(16)
+	opts.WithPassLength(128)
 	opts.WithUpperCase(true)
-	opts.WithMaxSpecialCharacters(2)
-	println(opts.GeneratePassword())
+	opts.WithMaxSpecialCharacters(10)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// GeneratePassword
+		start1 := time.Now()
+		println(opts.GeneratePassword())
+		println("function1) elapsed: ", time.Since(start1).String())
+		println()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// GeneratePasswordV3
+		start3 := time.Now()
+		println(opts.GeneratePasswordV3())
+		println("function3) elapsed: ", time.Since(start3).String())
+		println()
+	}()
+	wg.Wait()
 }
